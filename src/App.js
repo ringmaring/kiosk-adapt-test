790fef15f80b936d89531d7f9311194754e8ebde
ringmaring <imringma@naver.com>
2025-05-25 17:35:03 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>키오스크 적응력 테스트</h1>
+      <p>이제 제대로 작동합니다! 🎉</p>
+    </div>
+  );
+}
